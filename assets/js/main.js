efabb8db77f629f5412d06e5905f64fc4abb655e
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-12 15:53:33 +0100

    Inizio sviluppo js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('ok');
+var selectionMenu = $('.navbar-nav > li.dropdown')
+var selectionDropdown = $('.navbar-nav > li.dropdown > ul.dropdown-menu')
+
+selectionMenu.mouseenter(function(){
+  selectionDropdown.addClass('active');
+})
+selectionMenu.mouseleave(function(){
+  selectionDropdown.removeClass('active');
+})
