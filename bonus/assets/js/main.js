4c57591a75ce7ca4b2b6b0f3a957494f44d3b762
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-12 16:33:29 +0100

    Realizzazione navbar bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,9 +1,11 @@
-var selectionMenu = $('.navbar-nav > li.dropdown')
-var selectionDropdown = $('.navbar-nav > li.dropdown > ul.dropdown-menu')
+// var selectionMenu = $('.navbar-nav > li.dropdown')
+// var selectionDropdown = $('.navbar-nav > li.dropdown > ul.dropdown-menu')
+//
+// selectionMenu.mouseenter(function(){
+//   $(this).children().addClass('active');
+// })
+// selectionMenu.mouseleave(function(){
+//   $(this).children().removeClass('active');
+// })
 
-selectionMenu.mouseenter(function(){
-  $(this).children().addClass('active');
-})
-selectionMenu.mouseleave(function(){
-  $(this).children().removeClass('active');
-})
+console.log('ok');
