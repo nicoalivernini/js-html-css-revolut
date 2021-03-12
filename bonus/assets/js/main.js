a9f8c0e8d011a39687752d076f5601d823d715f3
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-12 18:44:18 +0100

    Sistemazione file

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,11 +1,9 @@
- var selectionMenu = $('.list-menu li')
- var selectionDropdown = $('.drop-menu')
+var selectionMenu = $('.navbar-nav > li.dropdown')
+var selectionDropdown = $('.navbar-nav > li.dropdown > ul.dropdown-menu')
 
- selectionMenu.mouseenter(function(){
-   $(this).children().addClass('active');
- })
- selectionMenu.mouseleave(function(){
-   $(this).children().removeClass('active');
- })
-
-console.log('ok');
+selectionMenu.mouseenter(function(){
+  $(this).children().addClass('active');
+})
+selectionMenu.mouseleave(function(){
+  $(this).children().removeClass('active');
+})
