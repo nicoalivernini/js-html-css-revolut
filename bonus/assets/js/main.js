8cfde694d4926f53846116b758b84f81e35c3aac
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-12 17:03:48 +0100

    Js implementato nel bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,11 +1,11 @@
-// var selectionMenu = $('.navbar-nav > li.dropdown')
-// var selectionDropdown = $('.navbar-nav > li.dropdown > ul.dropdown-menu')
-//
-// selectionMenu.mouseenter(function(){
-//   $(this).children().addClass('active');
-// })
-// selectionMenu.mouseleave(function(){
-//   $(this).children().removeClass('active');
-// })
+ var selectionMenu = $('.list-menu li')
+ var selectionDropdown = $('.drop-menu')
+
+ selectionMenu.mouseenter(function(){
+   $(this).children().addClass('active');
+ })
+ selectionMenu.mouseleave(function(){
+   $(this).children().removeClass('active');
+ })
 
 console.log('ok');
